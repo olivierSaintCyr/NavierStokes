997dd20e832004e27373cf613d3a92db846a836f
olivierSaintCyr <olivier@WhiteShadow.localdomain>
2020-12-23 22:27:03 -0500

    Dynamic pressure heat map added

--- src/main.py
+++ src/main.py
@@ -27,16 +27,18 @@ class Figure:
     def __init__(self, fluid : Fluid):
         self.fluid = fluid
 
-        self.fig, self.ax = plt.subplots()
+        self.fig, self.ax = plt.subplots(nrows=1, ncols=2)
 
         self.x, self.y = np.meshgrid(np.linspace(0,2*np.pi,self.fluid.shape[0]) ,
                                      np.linspace(0,2*np.pi,self.fluid.shape[1]))
         
-        self.quiver = self.ax.quiver(self.x, self.y, self.fluid.U, self.fluid.V)
+        self.quiver = self.ax[0].quiver(self.x, self.y, self.fluid.U, self.fluid.V)
+        self.pressure_hm = self.ax[1].imshow(NSE.dynamic_pressure(self.fluid.U, self.fluid.V, self.fluid.density), cmap='magma')
 
     def update_plot(self, Q):
         self.fluid.update_UV()
         self.quiver.set_UVC(self.fluid.U, self.fluid.V)
+        self.pressure_hm.set_data(NSE.dynamic_pressure(self.fluid.U, self.fluid.V, self.fluid.density))
         return self.quiver,
     
     def tight_layout(self):
@@ -44,10 +46,10 @@ class Figure:
     
     
 if __name__ == "__main__":
-    shape = (70,70)
+    shape = (200,200)
     delta_x, delta_y = 2*np.pi/shape[0], 2*np.pi/shape[1]
     delta_t = 0.01
-    viscosity = 0.001667
+    viscosity = 0.00005
     density = 1.0
 
     fluid = Fluid(shape, delta_x, delta_y, delta_t, density, viscosity)
